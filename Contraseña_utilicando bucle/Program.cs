e7d1c3be1d821079e0b157e9f508d3611cfb86f5
Alejandra Valdez <valdezmoralesalejandramaciel@gmail.com>
2024-02-18 20:14:27 -0500

    Agregar archivos de proyecto.

---
+++ Contraseña_utilicando bucle/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Contraseña");
+Console.WriteLine();
+
+{
+
+    string contraseñaCorrecta = "Alejandra";
+
+
+    int intentos = 3;
+
+    while (intentos > 0)
+    {
+
+        Console.WriteLine("Ingrese la contraseña:");
+        string contraseñaIngresada = Console.ReadLine();
+
+        if (contraseñaIngresada == contraseñaCorrecta)
+        {
+            Console.WriteLine("Contraseña correcta.");
+            break;
+        }
+        else
+        {
+            intentos--;
+            if (intentos > 0)
+            {
+                Console.WriteLine("La contraseña esta incorrecta. Le quedan " + intentos + " intentos.");
+            }
+            else
+            {
+                Console.WriteLine("se han agotado los numeros de intentos.");
+            }
+        }
+    }
+}
